eac9c193e6b7d7b16b69d75e8120074b717157e0
ian-c-hamilton <ihamilton7.ih.ih@gmail.com>
2024-10-22 13:45:58 -0700

    example 0.4

--- 0 - Randomness/randomGaussian.py
+++ 0 - Randomness/randomGaussian.py
@@ -3,18 +3,21 @@ import py5
 def settings():
     global width
     global height
-    width = 800
-    height = 800
+    width = 640
+    height = 240
     py5.size(width, height)
 
 def setup():
-    global w1
     py5.rect_mode(py5.CENTER)
     py5.background(255)
 
 
 def draw():
-    w1.step()
-    w1.show()
+    x = py5.random_gaussian(320, 60)
+    py5.no_stroke()
+    py5.fill(0, 10)
+    py5.circle(x, 120, 16)
+
+
 
 py5.run_sketch()
